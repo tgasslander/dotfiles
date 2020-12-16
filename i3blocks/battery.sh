5048d3d31bb00d0a8811357f96e8beadea946610
Tobias Gasslander <tobias@gasslander.com>
2020-12-16 22:55:42 +0100

    Charging icon

--- i3blocks/battery.sh
+++ i3blocks/battery.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 BAT=$(acpi -b | awk 'NR==1' | grep -E -o '[0-9][0-9]?%')
+CHRG=$(acpi -b | awk 'NR==1{print $3;exit}')
+if [ $CHRG == "Charging," ]; then
+  CHARGING=
+fi
 
 # DO NOT edit these. They contain font awesome icons that
 # may not be rendered in some terminals (I'm looking at you xterm)
@@ -22,30 +26,31 @@ GE_FULL=$(($LE_GOOD+1))
 LE_FULL=100
 
 COLOR_WARN="#FF8000"
-COLOR_LOW="#ff9000"
+COLOR_LOW="#FF9000"
 COLOR_HALF="#FFFF99"
 COLOR_GOOD="#FFFFFF"
 COLOR_FULL="#FFFFFF"
 
+
 VAL=${BAT%?}
 if [ "$VAL" -ge 0 ] && [ "$VAL" -le $LE_WARN ]; then
-  L1="$WARN $VAL" 
+  L1="$CHARGING $WARN $VAL" 
   L2=$L1
   L3=$COLOR_WARN
 elif [ "$VAL" -ge $GE_LOW ] &&  [ "$VAL" -le $LE_LOW ]; then
-  L1="$LOW $VAL" 
+  L1="$CHARGING $LOW $VAL" 
   L2=$L1
   L3=$COLOR_LOW
 elif [ "$VAL" -ge $GE_HALF ] &&  [ "$VAL" -le $LE_HALF ]; then
-  L1="$HALF $VAL"
+  L1="$CHARGING $HALF $VAL"
   L2=$L1
   L3=$COLOR_HALF
 elif [ "$VAL" -ge $GE_GOOD ] &&  [ "$VAL" -le $LE_GOOD ]; then
-  L1="$GOOD $VAL"
+  L1="$CHARGING $GOOD $VAL"
   L1=$L1
   L3=$COLOR_GOOD
 elif [ "$VAL" -ge $GE_FULL ] &&  [ "$VAL" -le $LE_FULL ]; then
-  L1="$FULL $VAL"
+  L1="$CHARGING $FULL $VAL"
   L2=$L1
   L3=$COLOR_FULL
 fi
