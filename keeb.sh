8fe96accd93f915eb4dbfd6662da170fe54d82b2
tgasslander <tobias@gasslander.com>
2021-07-21 20:56:55 +0200

    Added golang paths to .zshrc Added keeb.sh to switch keyboard layouts easier

---
+++ keeb.sh
@@ -0,0 +1,19 @@
+#!/bin/zsh
+
+if [ $# -eq 0 ]
+then
+	echo "No arguments! 'se' or 'us' accepted values"
+fi
+
+case "$1" in
+	se)
+		setxkbmap -layout se,se
+		echo "Keymap set to swedish"
+		;;
+
+	us)
+		setxkbmap -layout us,us
+		echo "Keymap set to us"
+		;;
+
+esac
