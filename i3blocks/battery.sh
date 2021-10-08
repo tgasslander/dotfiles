a0937089011c607493d34200fcd0abaff3986746
tgasslander <tobias@gasslander.com>
2021-10-08 08:10:33 +0200

    Correct the extraction of the battery status

--- i3blocks/battery.sh
+++ i3blocks/battery.sh
@@ -4,7 +4,7 @@
 # Make sure to edit this file with an editor
 # that can display font awesome
 
-BAT=$(acpi -b | awk 'NR==1' | grep -E -o '[0-9][0-9]?%')
+BAT=$(acpi -b | awk 'NR==2' | grep -E -o '[0-9][0-9]?%')
 
 # DO NOT edit these. They contain font awesome icons that
 # may not be rendered in some terminals (I'm looking at you xterm)
