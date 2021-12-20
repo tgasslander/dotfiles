2247a98a2df1840ddfa0aefa2f86814b56c99d72
tgasslander <tobias@gasslander.com>
2021-12-20 20:40:37 +0100

    Remove gutentag
    Automatic tag handling.
    Hogged my CPU!

--- nvim/init.lua
+++ nvim/init.lua
@@ -20,7 +20,6 @@ require('packer').startup(function()
   use 'tpope/vim-fugitive' -- Git commands in nvim
   use 'tpope/vim-rhubarb' -- Fugitive-companion to interact with github
   use 'tpope/vim-commentary' -- "gc" to comment visual regions/lines
-  use 'ludovicchabant/vim-gutentags' -- Automatic tags management
   -- UI to select things (files, grep results, open buffers...)
   use { 'nvim-telescope/telescope.nvim', requires = { 'nvim-lua/plenary.nvim' } }
   use 'joshdick/onedark.vim' -- Theme inspired by Atom
