12c3a861300d5f206d76829ab7e64d4f1f19a33a
tgasslander <tobias@gasslander.com>
2021-12-25 23:31:20 +0100

    Add 'gopls' support

--- nvim/init.lua
+++ nvim/init.lua
@@ -235,7 +235,7 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 
 -- Enable the following language servers
-local servers = { 'clangd', 'rust_analyzer', 'pyright', 'tsserver', 'sumneko_lua' }
+local servers = { 'clangd', 'rust_analyzer', 'pyright', 'tsserver', 'sumneko_lua', 'gopls'}
 for _, lsp in ipairs(servers) do
   nvim_lsp[lsp].setup {
     on_attach = on_attach,
