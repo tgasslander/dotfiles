c334579695c299ba0c133e1837a4d9f6d3742c13
tgasslander <tobias@gasslander.com>
2022-02-01 23:26:16 +0100

    tweak packer install

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,8 +1,9 @@
--- Install packer
+--- Install packer
 local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
 
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
   vim.fn.execute('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
+
 end
 
 vim.cmd [[
@@ -15,6 +16,7 @@ vim.cmd [[
 
 local use = require('packer').use
 require('packer').startup(function()
+
   use 'wbthomason/packer.nvim' -- Package manager
   use 'tomasiser/vim-code-dark'
   use 'preservim/nerdtree'
