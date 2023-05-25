54241d21222465358106dc80587f862d08ce7052
tgasslander <tobias@gasslander.com>
2023-05-25 23:36:26 +0200

    adapt to using thunderbolt with a new dock
    the whole thunderbolt setup is kinda flawed so
    don't expect this file to make you much happier

--- thunderbolt.sh
+++ thunderbolt.sh
@@ -1,5 +1,5 @@
 # toga - office setup
 # force the external monitor to change resolution to kick start it when it won't start...
-xrandr --output DP-0 --mode 1920x1080 && \
+xrandr --output DP-1.1 --mode 1920x1080 && \
 sleep 5 && \
-xrandr --output DP-3 --auto --output DP-0 --auto --right-of DP-3
+xrandr --output DP-3 --auto --output DP-1.1 --auto --right-of DP-3
