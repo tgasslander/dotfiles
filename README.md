e8a0790684a1ff7627f61eb0fc5ccae07452ed47
tgasslander <tobias@gasslander.com>
2024-08-16 23:14:26 +0200

    initial starship config

--- README.md
+++ README.md
@@ -1,29 +1,21 @@
 # dotfiles
+
 Well. My dotfiles.
-Only tested on Kubuntu 20.04 in i3wm.
-Being tested on MacOS.
+Tested on Kubuntu 22.04
+and MacOS.
 
-## Dependencies
-- i3wm
-- i3blocks
-- fontawesome
-- zsh
-- oh-my-szh
+> MacOS requires a bit of manual setup for the depenedencies.
+> Check the `env-install` repo for some hints
 
-## vim
-No longer being actively used so the .vimrc is gonna stay as is.
+## Installation
 
-## nvim
-A bunch of plugins and settings that makes an IDE of nvim in `nvim/`.
+Clone the `env-install` repo and run the install script.
 
-### Prerequisites
+### Mac Prerequisites
+
 Gotta install a patched font to work with the devicons.
+
 ```
 brew tap homebrew/cask-fonts
 brew install --cask font-hack-nerd-font
-````
-
-#### Install the Powerlevel ZSH theme
-```
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 ```
--- starship/.config/starship/custom.git.sh
+++ starship/.config/starship/custom.git.sh
@@ -4,11 +4,35 @@ gbr=$(printf %s "$gbr" | grep '*' | sed 's/*\ //g')
 # local gst
 gst=$(LC_ALL=C git status 2>&1)
 if printf %s "$gst" | grep -q "nothing to commit"; then
-	echo -e "\01\e[1m\02@\01\e[0;32m\0002$gbr\01\e[0m\02"
-elif printf %s "$gst" | grep -q "nothing added to commit"; then # Untracked
-	echo -e "\01\e[1m\02@\01\e[0;35m\0002$gbr\01\e[0m\02"
+	echo -e """\
+\e[48;2;166;209;137m\
+\e[38;2;54;58;79m \uf09b $gbr \
+\e[0m\
+\e[38;2;166;209;137m\ue0b4\e[0m"""
+elif printf %s "$gst" | grep -q "Changes to be committed"; then # Untracked
+	echo -e """\
+\e[48;2;231;130;132m\
+\e[38;2;54;58;79m \uf09b $gbr \
+\e[0m\
+\e[38;2;231;130;132m\
+\ue0b4\e[0m"""
+elif printf %s "$gst" | grep -q "Untracked files"; then # Untracked
+	echo -e """\
+\e[48;2;237;135;150m\
+\e[38;2;54;58;79m \uf09b $gbr \
+\e[0m\
+\e[38;2;237;135;150m\
+\ue0b4\e[0m"""
 elif printf %s "$gst" | grep -q "must be run in a work tree"; then # Not in work tree
 	echo -e "\01\e[1m\02@\01\e[0;37m\0002$gbr\01\e[0m\02"
-else  # Change not added/not merged yet
-	echo -e "\01\e[1m\02@\01$IRED\0002$gbr\01\e[0m\02"
+elif printf %s "$gst" | grep -q "Changes not staged for commit"; then # Local changes
+	echo -e """\
+\e[48;2;238;212;159m\
+\e[38;2;54;58;79m \uf09b $gbr \
+\e[0m\
+\e[38;2;238;212;159m\
+\ue0b4\e[0m"""
+else  # probably not a git repo
+	# echo -e "\e[38;2;238;212;159m\ue0b4"
+	echo -e "\e[38;2;140;170;238m\ue0b4"
 fi
