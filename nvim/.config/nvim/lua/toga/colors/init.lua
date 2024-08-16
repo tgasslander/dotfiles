6e56f815cb08587ff75de907585bb012a8a6a5a5
tgasslander <tobias@gasslander.com>
2024-08-16 16:12:40 +0200

    switch nvim colors to catppuccin macchiato

--- nvim/.config/nvim/lua/toga/colors/init.lua
+++ nvim/.config/nvim/lua/toga/colors/init.lua
@@ -1,5 +1,4 @@
 -- Set colorscheme (order is important here)
 vim.o.termguicolors = true
 vim.g.onedark_terminal_italics = 2
-vim.g.vscode_style = "dark"
-vim.cmd([[colorscheme vscode]])
+vim.cmd([[colorscheme catppuccin]])
