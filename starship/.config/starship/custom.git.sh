ccc48f766417f4da1c087c0a87dd3a6629f6e63e
tgasslander <tobias@gasslander.com>
2024-08-22 00:52:05 +0200

    adapt to work on mac
    tweak the control chars from \e to \033
    
    change the logic to not return when `git branch` returns
    an error

--- starship/.config/starship/custom.git.sh
+++ starship/.config/starship/custom.git.sh
@@ -1,38 +1,34 @@
-#!/bin/bash
-gbr=$(git branch 2>/dev/null) || return 1
-gbr=$(printf %s "$gbr" | grep '*' | sed 's/*\ //g')
+#!/bin/zsh
 # local gst
 gst=$(LC_ALL=C git status 2>&1)
+gbr=$(git branch 2>/dev/null)
+gbr=$(printf %s "$gbr" | grep '*' | sed 's/*\ //g')
 if printf %s "$gst" | grep -q "nothing to commit"; then
 	echo -e """\
-\e[48;2;166;209;137m\
-\e[38;2;54;58;79m \uf09b $gbr \
-\e[0m\
-\e[38;2;166;209;137m\ue0b4\e[0m"""
+\033[48;2;166;209;137m\
+\033[38;2;54;58;79m \uf09b $gbr \
+\033[0m\
+\033[38;2;166;209;137m\ue0b4\033[0m"""
 elif printf %s "$gst" | grep -q "Changes to be committed"; then # Untracked
 	echo -e """\
-\e[48;2;231;130;132m\
-\e[38;2;54;58;79m \uf09b $gbr \
-\e[0m\
-\e[38;2;231;130;132m\
-\ue0b4\e[0m"""
-elif printf %s "$gst" | grep -q "Untracked files"; then # Untracked
-	echo -e """\
-\e[48;2;237;135;150m\
-\e[38;2;54;58;79m \uf09b $gbr \
-\e[0m\
-\e[38;2;237;135;150m\
-\ue0b4\e[0m"""
+\033[48;2;231;130;132m\
+\033[38;2;54;58;79m \uf09b $gbr \
+\033[0m\
+\033[38;2;231;130;132m\
+\ue0b4\033[0m"""
 elif printf %s "$gst" | grep -q "must be run in a work tree"; then # Not in work tree
-	echo -e "\01\e[1m\02@\01\e[0;37m\0002$gbr\01\e[0m\02"
+	echo -e "\01\033[1m\02@\01\033[0;37m\0002$gbr\01\033[0m\02"
 elif printf %s "$gst" | grep -q "Changes not staged for commit"; then # Local changes
 	echo -e """\
-\e[48;2;238;212;159m\
-\e[38;2;54;58;79m \uf09b $gbr \
-\e[0m\
-\e[38;2;238;212;159m\
-\ue0b4\e[0m"""
+\033[48;2;238;212;159m\
+\033[38;2;54;58;79m \uf09b $gbr \
+\033[0m\
+\033[38;2;238;212;159m\
+\ue0b4\033[0m"""
 else  # probably not a git repo
-	# echo -e "\e[38;2;238;212;159m\ue0b4"
-	echo -e "\e[38;2;140;170;238m\ue0b4"
+	echo -e """\
+\033[38;2;140;170;238m\
+\ue0b4\
+\033[0m"""
 fi
+return 0
