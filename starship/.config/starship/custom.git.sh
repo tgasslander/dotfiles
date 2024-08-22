9915679ae47b52645a8f135ff24ec7121fcf0ecc
tgasslander <tobias@gasslander.com>
2024-08-22 23:18:34 +0200

    compact the right end of the prompt

--- starship/.config/starship/custom.git.sh
+++ starship/.config/starship/custom.git.sh
@@ -6,20 +6,20 @@ gbr=$(printf %s "$gbr" | grep '*' | sed 's/*\ //g')
 if printf %s "$gst" | grep -q "nothing added to commit"; then
 	echo -e """\
 \033[48;2;166;209;137m\
-\033[38;2;54;58;79m \uf09b $gbr \
+\033[38;2;54;58;79m \uf09b $gbr\
 \033[0m\
 \033[38;2;166;209;137m\ue0b4\033[0m"""
 elif printf %s "$gst" | grep -q "working tree clean"; then
 	echo -e """\
 \033[48;2;166;209;137m\
-\033[38;2;54;58;79m \uf09b $gbr \
+\033[38;2;54;58;79m \uf09b $gbr\
 \033[0m\
 \033[38;2;166;209;137m\ue0b4\033[0m"""
 e
 elif printf %s "$gst" | grep -q "Changes to be committed"; then # Untracked
 	echo -e """\
 \033[48;2;231;130;132m\
-\033[38;2;54;58;79m \uf09b $gbr \
+\033[38;2;54;58;79m \uf09b $gbr\
 \033[0m\
 \033[38;2;231;130;132m\
 \ue0b4\033[0m"""
@@ -28,7 +28,7 @@ elif printf %s "$gst" | grep -q "must be run in a work tree"; then # Not in work
 elif printf %s "$gst" | grep -q "Changes not staged for commit"; then # Local changes
 	echo -e """\
 \033[48;2;238;212;159m\
-\033[38;2;54;58;79m \uf09b $gbr \
+\033[38;2;54;58;79m \uf09b $gbr\
 \033[0m\
 \033[38;2;238;212;159m\
 \ue0b4\033[0m"""
