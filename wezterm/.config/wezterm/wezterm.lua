0108a63b34e6c2bf2574bed24a104f1e0ae791cc
tgasslander <tobias@gasslander.com>
2024-12-05 12:40:23 +0100

    add larger default window size

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -6,6 +6,8 @@ config.color_scheme = "catppuccin-frappe"
 config.enable_tab_bar = false -- We don't do tabs because we have tmux, tiling wm, etc.
 config.window_close_confirmation = "NeverPrompt" -- Exit without confirmation
 config.window_decorations = "RESIZE" -- Remove ugly decorations
+config.initial_rows = 64 -- Default rows
+config.initial_cols = 240 -- Default cols
 
 -- Avoid clashing with i3 (CMD|SHIFT+R to reload the i3 config)
 config.keys = {
