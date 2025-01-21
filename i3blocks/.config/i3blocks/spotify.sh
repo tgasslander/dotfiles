453ec8544b4b99e211ad3eab0583c38d94f64d53
tgasslander <tobias@gasslander.com>
2025-01-21 21:52:36 +0100

    fix a stability problem with dual sinks

--- i3blocks/.config/i3blocks/spotify.sh
+++ i3blocks/.config/i3blocks/spotify.sh
@@ -7,31 +7,31 @@ PAUSE_ICON=
 # Temp file to store last played song
 CACHE_FILE="/tmp/spotify_last_song"
 
-# Get Spotify sink input
-SPOTIFY_SINK=$(pactl list sink-inputs | grep -B 20 "application.name = \"Spotify\"" | grep "Sink Input #" | cut -d'#' -f2)
+# Get active (non-corked) Spotify sink input if it exists
+PLAYING_SINK=$(pactl list sink-inputs | grep -B 20 "application.name = \"Spotify\"" | grep -B 20 "Corked: no" | grep "Sink Input #" | cut -d'#' -f2)
 
-if [ -n "$SPOTIFY_SINK" ]; then
-    # Check if there's audio playing through the sink
-    RUNNING=$(pactl list sink-inputs | grep -A 20 "Sink Input #${SPOTIFY_SINK}" | grep "Corked: " | cut -d' ' -f2)
+if [ -n "$PLAYING_SINK" ]; then
+    # If we found a playing sink, get and show current song
+    TITLE=$(qdbus org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Metadata | grep "title:" | cut -d' ' -f2-)
+    ARTIST=$(qdbus org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Metadata | grep "artist:" | cut -d' ' -f2-)
+    CURRENT_SONG="$TITLE - $ARTIST"
     
-    if [ "$RUNNING" = "no" ]; then
-        # Get current song info
-        TITLE=$(qdbus org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Metadata | grep "title:" | cut -d' ' -f2-)
-        ARTIST=$(qdbus org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Metadata | grep "artist:" | cut -d' ' -f2-)
-        CURRENT_SONG="$TITLE - $ARTIST"
-        
-        # Save current song to cache file
-        echo "$CURRENT_SONG" > "$CACHE_FILE"
-        
-        # Output playing status
-        echo "$PLAY_ICON $CURRENT_SONG"
-    else
-        # Read last played song from cache if available
+    # Save current song to cache file
+    echo "$CURRENT_SONG" > "$CACHE_FILE"
+
+    # Output playing status
+    echo "$PLAY_ICON $CURRENT_SONG"
+else
+    # Check if Spotify is running but paused
+    PAUSED_SINK=$(pactl list sink-inputs | grep -B 20 "application.name = \"Spotify\"" | grep "Sink Input #" | head -n 1 | cut -d'#' -f2)
+
+    if [ -n "$PAUSED_SINK" ]; then
+        # Spotify is running but paused
         if [ -f "$CACHE_FILE" ]; then
             LAST_SONG=$(cat "$CACHE_FILE")
-            echo "$PLAY_ICON $PAUSE_ICON $LAST_SONG"
+            echo "$PAUSE_ICON $LAST_SONG"
         else
-            echo "$PLAY_ICON $PAUSE_ICON paused"
+            echo "$PAUSE_ICON paused"
         fi
     fi
 fi
