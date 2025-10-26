3d91cb0936997fc7e3041f10e42f9a5c0af1b996
tgasslander <tobias@gasslander.com>
2025-10-26 21:18:45 +0100

    try a different home icon

--- i3blocks/.config/i3blocks/wireguard.sh
+++ i3blocks/.config/i3blocks/wireguard.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-IP_ADDRESS=$(hostname -I | awk '{print " " $1}')
+# IP_ADDRESS=$(hostname -I | awk '{print " " $1}')
+# IP_ADDRESS=$(hostname -I | awk '{print " " $1}')
+# IP_ADDRESS=$(hostname -I | awk '{print " " $1}')
+IP_ADDRESS=$(hostname -I | awk '{print " " $1}')
 
 if ip link show wg0 up &>/dev/null; then
     printf "\\UFE0E $IP_ADDRESS"
