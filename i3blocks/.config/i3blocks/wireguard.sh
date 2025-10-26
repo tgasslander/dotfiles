02d8adc5ce25948db59eba7ba163ff15b3945989
tgasslander <tobias@gasslander.com>
2025-10-26 21:24:20 +0100

    use only the relevant IP addr

--- i3blocks/.config/i3blocks/wireguard.sh
+++ i3blocks/.config/i3blocks/wireguard.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# IP_ADDRESS=$(hostname -I | awk '{print " " $1}')
-# IP_ADDRESS=$(hostname -I | awk '{print " " $1}')
-# IP_ADDRESS=$(hostname -I | awk '{print " " $1}')
-IP_ADDRESS=$(hostname -I | awk '{print " " $1}')
+# IP_ADDRESS=$(hostname -i | awk '{print " " $1}')
+# IP_ADDRESS=$(hostname -i | awk '{print " " $1}')
+# IP_ADDRESS=$(hostname -i | awk '{print " " $1}')
+IP_ADDRESS=$(hostname -i | awk '{print " " $1}')
 
 if ip link show wg0 up &>/dev/null; then
     printf "\\UFE0E $IP_ADDRESS"
