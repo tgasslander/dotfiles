c4a0b53f9b365704c7dac97aaf99e289f326f8aa
tgasslander <tobias@gasslander.com>
2025-12-12 19:04:36 +0100

    revert to getting all ip addresses

--- i3blocks/.config/i3blocks/wireguard.sh
+++ i3blocks/.config/i3blocks/wireguard.sh
@@ -3,7 +3,7 @@
 # IP_ADDRESS=$(hostname -i | awk '{print " " $1}')
 # IP_ADDRESS=$(hostname -i | awk '{print " " $1}')
 # IP_ADDRESS=$(hostname -i | awk '{print " " $1}')
-IP_ADDRESS=$(hostname -i | awk '{print " " $1}')
+IP_ADDRESS=$(hostname -I | awk '{print " " $1}')
 
 if ip link show wg0 up &>/dev/null; then
     printf "\\UFE0E $IP_ADDRESS"
