db280a976472e02ebc6b25224acce728863ece91
tgasslander <tobias@gasslander.com>
2026-06-05 20:51:23 +0200

    fix dev session tmux setup

--- scripts/.local/bin/tmux-dev-session.sh
+++ scripts/.local/bin/tmux-dev-session.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 tmux kill-session -t coder 2>/dev/null
 
-tmux new-session -s coder \; \
+tmux new-session -d -s coder \; \
   split-window -v -p 15 \; \
   select-pane -t 2 \; \
   split-window -h -p 67 \; \
@@ -9,10 +9,15 @@ tmux new-session -s coder \; \
   select-pane -t 1 \; \
   send-keys 'nvim .' Enter
 
-# tmux new-session -s coder \; \
-#   split-window -v -p 90 \; \
-#   select-pane -t 1 \; \
-#   split-window -h -p 67 \; \
-#   split-window -h -p 50 \; \
-#   select-pane -t 4 \; \
-#   send-keys 'nvim .' Enter
+tmux new-window -t coder -n "vibe"
+tmux new-window -t coder -n "terminal"
+
+# Prod monitoring
+tmux new-window -t coder -n k9s
+tmux send-keys -t coder:k9s 'KUBECONFIG=~/.kube/risiito-prod.yaml k9s' C-m
+
+# Talos local monitoring
+tmux new-window -t coder -n k9s
+tmux send-keys -t coder:k9s 'k9s' C-m
+
+tmux attach-session -t coder
